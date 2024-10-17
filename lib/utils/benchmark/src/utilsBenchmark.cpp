2613e3ba8607ccc011804924df573bc553541bdb
Samuele Pasini <sapa@bendingspoons.com>
2024-10-17 22:52:50 +0200

    Preliminary work on large keys

--- lib/utils/benchmark/src/utilsBenchmark.cpp
+++ lib/utils/benchmark/src/utilsBenchmark.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Samuele Pasini
+Copyright 2024 Massimo Fioravanti
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,146 +14,27 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include <benchmark/benchmark.h>
-#include <unordered_map>
-#include <map>
-#include <string>
-#include <iostream>
 #include <utility>
+
+#include "benchmark/benchmark.h"
 
 constexpr size_t minRange = 1 << 2;
 constexpr size_t maxRange = 1 << 10;
 
-// Template function for benchmarking
-template <typename PassedType>
-static void BM_SubsciptorOperatorInsert(benchmark::State& state) {
-    PassedType dictionary;
-
-    for (auto _ : state) {
+static void forCycleBenchmark(benchmark::State& state)
+{
+	int a = 0;
+	for (auto _ : state)
+	{
 		for (int i = 0; i < state.range(); i++)
- 			benchmark::DoNotOptimize(dictionary[i] = i);
-    }
-    state.SetComplexityN(state.range(0));
+			benchmark::DoNotOptimize(a += i);
+	}
+	state.SetComplexityN(state.range(0));
 }
 
-// Template function for benchmarking
-template <typename PassedType>
-static void BM_Insert(benchmark::State& state) {
-    PassedType dictionary;
+BENCHMARK(forCycleBenchmark)
+		->RangeMultiplier(2)
+		->Range(minRange, maxRange)
+		->Complexity();
 
-    for (auto _ : state) {
-		for (int i = 0; i < state.range(); i++)
- 			benchmark::DoNotOptimize(dictionary.insert(std::make_pair(i,i)));
-    }
-    state.SetComplexityN(state.range(0));
-}
-
-// Template function for benchmarking
-template <typename PassedType>
-static void BM_SubscriptorOperatorFind(benchmark::State& state) {
-    PassedType dictionary;
-    // Setup the dictionary
-    for (int i = 0; i < state.range(0); ++i) {
-        dictionary[i] = i;
-    }
-
-    for (auto _ : state) {
-		for (int i = 0; i < state.range(); i++)
- 			benchmark::DoNotOptimize(dictionary[i]);
-    }
-    state.SetComplexityN(state.range(0));
-}
-
-// Template function for benchmarking
-template <typename PassedType>
-static void BM_Find(benchmark::State& state) {
-    PassedType dictionary;
-    // Setup the dictionary
-    for (int i = 0; i < state.range(0); ++i) {
-        dictionary[i] = i;
-    }
-
-    for (auto _ : state) {
-		for (int i = 0; i < state.range(); i++)
- 			benchmark::DoNotOptimize(dictionary.find(i));
-    }
-    state.SetComplexityN(state.range(0));
-}
-
-// Template function for benchmarking
-template <typename PassedType>
-static void BM_Erase(benchmark::State& state) {
-    PassedType dictionary;
-    // Setup the dictionary
-    for (int i = 0; i < state.range(0); ++i) {
-        dictionary[i] = i;
-    }
-
-    for (auto _ : state) {
-		for (int i = 0; i < state.range(); i++)
- 			benchmark::DoNotOptimize(dictionary.erase(i));
-    }
-    state.SetComplexityN(state.range(0));
-}
-
-// Register template with a specific class based on a string input
-template <typename PassedType>
-void RegisterBenchmarkSuite(const std::string& name) {
-    std::string benchmark_name = "BM_SubscriptOperatorInsert_" + name;
-    BENCHMARK_TEMPLATE(BM_SubsciptorOperatorInsert, PassedType)
-        ->Name(benchmark_name.c_str())
-        ->RangeMultiplier(2)
-        ->Range(minRange, maxRange)
-        ->Complexity();
-
-	benchmark_name = "BM_Insert_" + name;
-    BENCHMARK_TEMPLATE(BM_Insert, PassedType)
-        ->Name(benchmark_name.c_str())
-        ->RangeMultiplier(2)
-        ->Range(minRange, maxRange)
-        ->Complexity();
-
-	benchmark_name = "BM_SubscriptorOperatorFind_" + name;
-    BENCHMARK_TEMPLATE(BM_SubscriptorOperatorFind, PassedType)
-        ->Name(benchmark_name.c_str())
-        ->RangeMultiplier(2)
-        ->Range(minRange, maxRange)
-        ->Complexity();
-	
-	benchmark_name = "BM_Find_" + name;
-    BENCHMARK_TEMPLATE(BM_Find, PassedType)
-        ->Name(benchmark_name.c_str())
-        ->RangeMultiplier(2)
-        ->Range(minRange, maxRange)
-        ->Complexity();
-	
-	benchmark_name = "BM_Erase_" + name;
-    BENCHMARK_TEMPLATE(BM_Erase, PassedType)
-        ->Name(benchmark_name.c_str())
-        ->RangeMultiplier(2)
-        ->Range(minRange, maxRange)
-        ->Complexity();
-}
-
-int main(int argc, char** argv) {
-    // Default to "all" if no argument is provided
-    std::string choice = (argc > 1) ? argv[argc-1] : "all";
-
-    // Register the benchmark based on user input
-	if (choice == "all" or choice[0] == '-') {
-		RegisterBenchmarkSuite<std::unordered_map<int, int>>("unordered_map");
-		RegisterBenchmarkSuite<std::map<int, int>>("map");
-	}else if (choice == "unordered_map") {
-        RegisterBenchmarkSuite<std::unordered_map<int, int>>("unordered_map");
-    } else if (choice == "map") {
-        RegisterBenchmarkSuite<std::map<int, int>>("map");
-    } else {
-        std::cerr << "Invalid choice. Use <executable> [flags] [all|unordered_map|map].\n";
-        return 1;
-    }
-
-    // Initialize and run benchmarks
-    ::benchmark::Initialize(&argc, argv);
-    ::benchmark::RunSpecifiedBenchmarks();
-	::benchmark::Shutdown();  
-}
+BENCHMARK_MAIN();
