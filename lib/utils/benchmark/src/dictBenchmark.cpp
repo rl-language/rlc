be8510ef1dcf7e9c402acf6aa07b4c0b742d125b
Samuele Pasini <sapa@bendingspoons.com>
2024-10-19 19:05:22 +0200

    feat: add randomness to tests

--- lib/utils/benchmark/src/dictBenchmark.cpp
+++ lib/utils/benchmark/src/dictBenchmark.cpp
@@ -15,11 +15,19 @@ limitations under the License.
 */
 
 #include <benchmark/benchmark.h>
+
+#include <algorithm> // For std::shuffle
 #include <unordered_map>
+#include <unordered_set>
+#include <vector>
 #include <map>
 #include <string>
 #include <iostream>
 #include <utility>
+#include <random>
+
+std::random_device rd;
+std::mt19937 gen(rd());
 
 constexpr size_t minRange = 1 << 2;
 constexpr size_t maxRange = 1 << 10;
@@ -45,7 +53,22 @@ namespace std {
     };
 }
 
-// Template function for benchmarking
+std::vector<int> GenerateNRandomIntegers(int n){
+    std::uniform_int_distribution<> distrib(-n, n);
+
+    std::unordered_set<int> unique_numbers;
+
+    // Generate unique numbers
+    while (unique_numbers.size() < static_cast<size_t>(n)) {
+        unique_numbers.insert(distrib(gen));
+    }
+
+    std::vector<int> result(unique_numbers.begin(), unique_numbers.end());
+
+    return result;
+}
+
+
 template <typename DictType, typename KeyType>
 static void BM_SubsciptorOperatorInsert(benchmark::State& state) {
     DictType dictionary;
@@ -53,6 +76,18 @@ static void BM_SubsciptorOperatorInsert(benchmark::State& state) {
     for (auto _ : state) {
 		for (int i = 0; i < state.range(); i++)
  			dictionary[KeyType(i)] = i;
+    }
+    state.SetComplexityN(state.range(0));
+}
+
+template <typename DictType, typename KeyType>
+static void BM_SubsciptorOperatorInsertRandom(benchmark::State& state) {
+    DictType dictionary;
+    std::vector<int> unique_numbers = GenerateNRandomIntegers(state.range());
+
+    for (auto _ : state) {
+		for (int i = 0; i < state.range(); i++)
+ 			dictionary[KeyType(unique_numbers[i])] = i;
     }
     state.SetComplexityN(state.range(0));
 }
@@ -65,6 +100,19 @@ static void BM_Insert(benchmark::State& state) {
     for (auto _ : state) {
 		for (int i = 0; i < state.range(); i++)
  			benchmark::DoNotOptimize(dictionary.insert(std::make_pair(KeyType(i),i)));
+    }
+    state.SetComplexityN(state.range(0));
+}
+
+// Template function for benchmarking
+template <typename PassedType, typename KeyType>
+static void BM_InsertRandom(benchmark::State& state) {
+    PassedType dictionary;
+    std::vector<int> unique_numbers = GenerateNRandomIntegers(state.range());
+
+    for (auto _ : state) {
+		for (int i = 0; i < state.range(); i++)
+ 			benchmark::DoNotOptimize(dictionary.insert(std::make_pair(KeyType(unique_numbers[i]),i)));
     }
     state.SetComplexityN(state.range(0));
 }
@@ -87,6 +135,26 @@ static void BM_SubscriptorOperatorFind(benchmark::State& state) {
 
 // Template function for benchmarking
 template <typename PassedType, typename KeyType>
+static void BM_SubscriptorOperatorFindRandom(benchmark::State& state) {
+    PassedType dictionary;
+    std::vector<int> unique_numbers = GenerateNRandomIntegers(state.range());
+
+    // Setup the dictionary
+    for (int i = 0; i < state.range(0); ++i) {
+        dictionary[KeyType(unique_numbers[i])] = i;
+    }
+
+    std::shuffle(unique_numbers.begin(), unique_numbers.end(), gen);
+
+    for (auto _ : state) {
+		for (int i = 0; i < state.range(); i++)
+ 			benchmark::DoNotOptimize(dictionary[KeyType(unique_numbers[i])]);
+    }
+    state.SetComplexityN(state.range(0));
+}
+
+// Template function for benchmarking
+template <typename PassedType, typename KeyType>
 static void BM_Find(benchmark::State& state) {
     PassedType dictionary;
     // Setup the dictionary
@@ -97,6 +165,26 @@ static void BM_Find(benchmark::State& state) {
     for (auto _ : state) {
 		for (int i = 0; i < state.range(); i++)
  			benchmark::DoNotOptimize(dictionary.find(KeyType(i)));
+    }
+    state.SetComplexityN(state.range(0));
+}
+
+// Template function for benchmarking
+template <typename PassedType, typename KeyType>
+static void BM_FindRandom(benchmark::State& state) {
+    PassedType dictionary;
+    std::vector<int> unique_numbers = GenerateNRandomIntegers(state.range());
+
+    // Setup the dictionary
+    for (int i = 0; i < state.range(0); ++i) {
+        dictionary[KeyType(unique_numbers[i])] = i;
+    }
+
+    std::shuffle(unique_numbers.begin(), unique_numbers.end(), gen);
+
+    for (auto _ : state) {
+		for (int i = 0; i < state.range(); i++)
+ 			benchmark::DoNotOptimize(dictionary.find(KeyType(unique_numbers[i])));
     }
     state.SetComplexityN(state.range(0));
 }
@@ -117,6 +205,26 @@ static void BM_Erase(benchmark::State& state) {
     state.SetComplexityN(state.range(0));
 }
 
+// Template function for benchmarking
+template <typename PassedType, typename KeyType>
+static void BM_EraseRandom(benchmark::State& state) {
+    PassedType dictionary;
+    std::vector<int> unique_numbers = GenerateNRandomIntegers(state.range());
+
+    // Setup the dictionary
+    for (int i = 0; i < state.range(0); ++i) {
+        dictionary[KeyType(unique_numbers[i])] = i;
+    }
+
+    std::shuffle(unique_numbers.begin(), unique_numbers.end(), gen);
+
+    for (auto _ : state) {
+		for (int i = 0; i < state.range(); i++)
+ 			benchmark::DoNotOptimize(dictionary.erase(KeyType(unique_numbers[i])));
+    }
+    state.SetComplexityN(state.range(0));
+}
+
 
 // Register template with a specific class based on a string input
 template <typename PassedType, typename KeyType>
@@ -128,8 +236,22 @@ void RegisterBenchmarkSuite(const std::string& name) {
         ->Range(minRange, maxRange)
         ->Complexity();
 
+    benchmark_name = "BM_SubscriptOperatorInsert_Random_" + name;
+    BENCHMARK_TEMPLATE(BM_SubsciptorOperatorInsertRandom, PassedType, KeyType)
+        ->Name(benchmark_name.c_str())
+        ->RangeMultiplier(2)
+        ->Range(minRange, maxRange)
+        ->Complexity();
+
 	benchmark_name = "BM_Insert_" + name;
     BENCHMARK_TEMPLATE(BM_Insert, PassedType, KeyType)
+        ->Name(benchmark_name.c_str())
+        ->RangeMultiplier(2)
+        ->Range(minRange, maxRange)
+        ->Complexity();
+    
+    benchmark_name = "BM_Insert_Random_" + name;
+    BENCHMARK_TEMPLATE(BM_InsertRandom, PassedType, KeyType)
         ->Name(benchmark_name.c_str())
         ->RangeMultiplier(2)
         ->Range(minRange, maxRange)
@@ -141,9 +263,23 @@ void RegisterBenchmarkSuite(const std::string& name) {
         ->RangeMultiplier(2)
         ->Range(minRange, maxRange)
         ->Complexity();
+    
+    benchmark_name = "BM_SubscriptorOperatorFind_Random_" + name;
+    BENCHMARK_TEMPLATE(BM_SubscriptorOperatorFindRandom, PassedType, KeyType)
+        ->Name(benchmark_name.c_str())
+        ->RangeMultiplier(2)
+        ->Range(minRange, maxRange)
+        ->Complexity();
 	
 	benchmark_name = "BM_Find_" + name;
     BENCHMARK_TEMPLATE(BM_Find, PassedType, KeyType)
+        ->Name(benchmark_name.c_str())
+        ->RangeMultiplier(2)
+        ->Range(minRange, maxRange)
+        ->Complexity();
+    
+    benchmark_name = "BM_Find_Random_" + name;
+    BENCHMARK_TEMPLATE(BM_FindRandom, PassedType, KeyType)
         ->Name(benchmark_name.c_str())
         ->RangeMultiplier(2)
         ->Range(minRange, maxRange)
@@ -155,10 +291,16 @@ void RegisterBenchmarkSuite(const std::string& name) {
         ->RangeMultiplier(2)
         ->Range(minRange, maxRange)
         ->Complexity();
+    
+    benchmark_name = "BM_Erase_Random_" + name;
+    BENCHMARK_TEMPLATE(BM_EraseRandom, PassedType, KeyType)
+        ->Name(benchmark_name.c_str())
+        ->RangeMultiplier(2)
+        ->Range(minRange, maxRange)
+        ->Complexity();
 }
 
 int main(int argc, char** argv) {
-    // Default to "all" if no argument is provided
     std::string typeChoice = "unordered_map";
     std::string keyChoice = "small";
 
@@ -179,18 +321,18 @@ int main(int argc, char** argv) {
     // Register the benchmark based on user input
     if (typeChoice == "unordered_map") {
         if(keyChoice == "large"){
-            RegisterBenchmarkSuite<std::unordered_map<LargeKey, int>, LargeKey>("unordered_map_large");
+            RegisterBenchmarkSuite<std::unordered_map<LargeKey, int>, LargeKey>("UnorderedMap_Large");
         } else if (keyChoice == "small"){
-            RegisterBenchmarkSuite<std::unordered_map<int, int>, int>("unordered_map_small");
+            RegisterBenchmarkSuite<std::unordered_map<int, int>, int>("UnorderedMap_Small");
         } else {
             std::cerr << "Invalid key choice. Use --key (small|large).\n";
             return 1;
         }
     } else if (typeChoice == "map") {
         if(keyChoice == "large"){
-            RegisterBenchmarkSuite<std::unordered_map<LargeKey, int>, LargeKey>("map_large");
+            RegisterBenchmarkSuite<std::unordered_map<LargeKey, int>, LargeKey>("Map_Large");
         } else if (keyChoice == "small"){
-            RegisterBenchmarkSuite<std::unordered_map<int, int>, int>("map_small");
+            RegisterBenchmarkSuite<std::unordered_map<int, int>, int>("Map_Small");
         } else {
             std::cerr << "Invalid key choice. Use --key (small|large).\n";
             return 1;
