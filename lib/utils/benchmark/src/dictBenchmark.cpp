9fba5a0a62732422b58c7591f327ae686b3edf6a
Samuele Pasini <sapa@bendingspoons.com>
2024-10-19 15:27:13 +0200

    feat: add large key for all benchmarks

--- lib/utils/benchmark/src/dictBenchmark.cpp
+++ lib/utils/benchmark/src/dictBenchmark.cpp
@@ -58,64 +58,65 @@ static void BM_SubsciptorOperatorInsert(benchmark::State& state) {
 }
 
 // Template function for benchmarking
-template <typename PassedType>
+template <typename PassedType, typename KeyType>
 static void BM_Insert(benchmark::State& state) {
     PassedType dictionary;
 
     for (auto _ : state) {
 		for (int i = 0; i < state.range(); i++)
- 			benchmark::DoNotOptimize(dictionary.insert(std::make_pair(i,i)));
+ 			benchmark::DoNotOptimize(dictionary.insert(std::make_pair(KeyType(i),i)));
     }
     state.SetComplexityN(state.range(0));
 }
 
 // Template function for benchmarking
-template <typename PassedType>
+template <typename PassedType, typename KeyType>
 static void BM_SubscriptorOperatorFind(benchmark::State& state) {
     PassedType dictionary;
     // Setup the dictionary
     for (int i = 0; i < state.range(0); ++i) {
-        dictionary[i] = i;
+        dictionary[KeyType(i)] = i;
     }
 
     for (auto _ : state) {
 		for (int i = 0; i < state.range(); i++)
- 			benchmark::DoNotOptimize(dictionary[i]);
+ 			benchmark::DoNotOptimize(dictionary[KeyType(i)]);
     }
     state.SetComplexityN(state.range(0));
 }
 
 // Template function for benchmarking
-template <typename PassedType>
+template <typename PassedType, typename KeyType>
 static void BM_Find(benchmark::State& state) {
     PassedType dictionary;
     // Setup the dictionary
     for (int i = 0; i < state.range(0); ++i) {
-        dictionary[i] = i;
+        dictionary[KeyType(i)] = i;
     }
 
     for (auto _ : state) {
 		for (int i = 0; i < state.range(); i++)
- 			benchmark::DoNotOptimize(dictionary.find(i));
+ 			benchmark::DoNotOptimize(dictionary.find(KeyType(i)));
     }
     state.SetComplexityN(state.range(0));
 }
 
 // Template function for benchmarking
-template <typename PassedType>
+template <typename PassedType, typename KeyType>
 static void BM_Erase(benchmark::State& state) {
     PassedType dictionary;
     // Setup the dictionary
     for (int i = 0; i < state.range(0); ++i) {
-        dictionary[i] = i;
+        dictionary[KeyType(i)] = i;
     }
 
     for (auto _ : state) {
 		for (int i = 0; i < state.range(); i++)
- 			benchmark::DoNotOptimize(dictionary.erase(i));
+ 			benchmark::DoNotOptimize(dictionary.erase(KeyType(i)));
     }
     state.SetComplexityN(state.range(0));
 }
+
 
 // Register template with a specific class based on a string input
 template <typename PassedType, typename KeyType>
@@ -127,49 +128,75 @@ void RegisterBenchmarkSuite(const std::string& name) {
         ->Range(minRange, maxRange)
         ->Complexity();
 
-	// benchmark_name = "BM_Insert_" + name;
-    // BENCHMARK_TEMPLATE(BM_Insert, PassedType)
-    //     ->Name(benchmark_name.c_str())
-    //     ->RangeMultiplier(2)
-    //     ->Range(minRange, maxRange)
-    //     ->Complexity();
+	benchmark_name = "BM_Insert_" + name;
+    BENCHMARK_TEMPLATE(BM_Insert, PassedType, KeyType)
+        ->Name(benchmark_name.c_str())
+        ->RangeMultiplier(2)
+        ->Range(minRange, maxRange)
+        ->Complexity();
 
-	// benchmark_name = "BM_SubscriptorOperatorFind_" + name;
-    // BENCHMARK_TEMPLATE(BM_SubscriptorOperatorFind, PassedType)
-    //     ->Name(benchmark_name.c_str())
-    //     ->RangeMultiplier(2)
-    //     ->Range(minRange, maxRange)
-    //     ->Complexity();
+	benchmark_name = "BM_SubscriptorOperatorFind_" + name;
+    BENCHMARK_TEMPLATE(BM_SubscriptorOperatorFind, PassedType, KeyType)
+        ->Name(benchmark_name.c_str())
+        ->RangeMultiplier(2)
+        ->Range(minRange, maxRange)
+        ->Complexity();
 	
-	// benchmark_name = "BM_Find_" + name;
-    // BENCHMARK_TEMPLATE(BM_Find, PassedType)
-    //     ->Name(benchmark_name.c_str())
-    //     ->RangeMultiplier(2)
-    //     ->Range(minRange, maxRange)
-    //     ->Complexity();
+	benchmark_name = "BM_Find_" + name;
+    BENCHMARK_TEMPLATE(BM_Find, PassedType, KeyType)
+        ->Name(benchmark_name.c_str())
+        ->RangeMultiplier(2)
+        ->Range(minRange, maxRange)
+        ->Complexity();
 	
-	// benchmark_name = "BM_Erase_" + name;
-    // BENCHMARK_TEMPLATE(BM_Erase, PassedType)
-    //     ->Name(benchmark_name.c_str())
-    //     ->RangeMultiplier(2)
-    //     ->Range(minRange, maxRange)
-    //     ->Complexity();
+	benchmark_name = "BM_Erase_" + name;
+    BENCHMARK_TEMPLATE(BM_Erase, PassedType, KeyType)
+        ->Name(benchmark_name.c_str())
+        ->RangeMultiplier(2)
+        ->Range(minRange, maxRange)
+        ->Complexity();
 }
 
 int main(int argc, char** argv) {
     // Default to "all" if no argument is provided
-    std::string choice = (argc > 1) ? argv[argc-1] : "all";
+    std::string typeChoice = "unordered_map";
+    std::string keyChoice = "small";
+
+    if (argc > 1){
+        for(int i = 1; i < argc - 1; i++){
+            std::string flag = argv[i];
+            if(flag == "--type"){
+                typeChoice = argv[i+1];
+                i++;
+            } else if(flag == "--key"){
+                keyChoice = argv[i+1];
+                i++;
+            }
+        }
+    }
+
 
     // Register the benchmark based on user input
-	if (choice == "all" or choice[0] == '-') {
-		RegisterBenchmarkSuite<std::unordered_map<int, int>, int>("unordered_map");
-		RegisterBenchmarkSuite<std::map<int, int>, int>("map");
-	}else if (choice == "unordered_map") {
-        RegisterBenchmarkSuite<std::unordered_map<int, int>, int>("unordered_map");
-    } else if (choice == "map") {
-        RegisterBenchmarkSuite<std::map<int, int>, int>("map");
+    if (typeChoice == "unordered_map") {
+        if(keyChoice == "large"){
+            RegisterBenchmarkSuite<std::unordered_map<LargeKey, int>, LargeKey>("unordered_map_large");
+        } else if (keyChoice == "small"){
+            RegisterBenchmarkSuite<std::unordered_map<int, int>, int>("unordered_map_small");
+        } else {
+            std::cerr << "Invalid key choice. Use --key (small|large).\n";
+            return 1;
+        }
+    } else if (typeChoice == "map") {
+        if(keyChoice == "large"){
+            RegisterBenchmarkSuite<std::unordered_map<LargeKey, int>, LargeKey>("map_large");
+        } else if (keyChoice == "small"){
+            RegisterBenchmarkSuite<std::unordered_map<int, int>, int>("map_small");
+        } else {
+            std::cerr << "Invalid key choice. Use --key (small|large).\n";
+            return 1;
+        }
     } else {
-        std::cerr << "Invalid choice. Use <executable> [flags] [all|unordered_map|map].\n";
+        std::cerr << "Invalid type choice. Use --type (unordered_map|map).\n";
         return 1;
     }
 
