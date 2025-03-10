8679358a77fed544bffe04be91ddda7da4f6fff9
Samuele Pasini <sapa@bendingspoons.com>
2025-03-10 18:13:27 +0100

    chore: update license

--- lib/utils/benchmark/src/DictIntBenchBenchmark.cpp
+++ lib/utils/benchmark/src/DictIntBenchBenchmark.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Massimo Fioravanti
+Copyright 2024 Samuele Pasini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
