777b5a386fc41e69f721c83847552d1c2e2f1232
Massimo Fioravanti <massimo.fioravanti@polimi.it>
2025-11-13 23:49:02 +0100

    0.5.10

--- python/setup.py
+++ python/setup.py
@@ -99,7 +99,7 @@ site_packages_path = (
     target_bin_dir if os.name != "nt" else os.path.join("Lib", "site-packages")
 )
 
-version="0.4.9"
+version="0.4.10"
 setup(
     name="rl_language_core",
     version=version,
